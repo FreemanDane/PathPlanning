229940b84332088823e4e13dbca1480ae147c2b7
Bill Jia <jia379853818@hotmail.com>
2017-06-28 16:58:21 +0800

    Map zoom demo update

--- src/map_zoom.py
+++ src/map_zoom.py
@@ -6,25 +6,44 @@ class thu_map(QGraphicsPixmapItem):
     def __init__(self):
         super(thu_map, self).__init__()
         self.setPixmap(QPixmap("../data/map.png"))
-        self.zoom = 0
+        self.size_x = 685
+        self.size_y = 831
+        self.zoom = [0, 0]           #第一个代表鼠标滚动之前（过去状态），第二个代表鼠标滚动之后（当前状态）
+        self.mouse = QPointF(0, 0)
+        self.top_left = QPointF(0, 0)
     def boundingRect(self):
-        return QRectF(0, 0, 685, 831)
+        return QRectF(self.top_left.x(), self.top_left.y(), self.size_x * pow(1.2, 0.5 * self.zoom[1]), self.size_y * pow(1.2, 0.5 * self.zoom[1]))
     def paint(self, painter, option, widget):
-        #self.show()
-        rectF = QRectF(0, 0, 685, 831)
-        painter.drawPixmap(rectF, QPixmap("../data/map.png"),rectF)
+        zoom_change = self.zoom[1] - self.zoom[0]
+        x = pow(1.2, 0.5 * zoom_change) * self.top_left.x() - (pow(1.2, 0.5 * zoom_change) - 1) * self.mouse.x()
+        if x < self.size_x * (1 - pow(1.2, 0.5 * self.zoom[1])):
+            x = self.size_x * (1 - pow(1.2, 0.5 * self.zoom[1]))
+        elif x > 0:
+            x = 0
+        y = pow(1.2, 0.5 * zoom_change) * self.top_left.y() - (pow(1.2, 0.5 * zoom_change) - 1) * self.mouse.y()
+        if y < self.size_y * (1 - pow(1.2, 0.5 * self.zoom[1])):
+            y = self.size_y * (1 - pow(1.2, 0.5 * self.zoom[1]))
+        elif y > 0:
+            y = 0
+        self.top_left = QPointF(x, y)
+        target = QRectF(x, y, self.size_x * pow(1.2, 0.5 * self.zoom[1]), self.size_y * pow(1.2, 0.5 * self.zoom[1]))
+        source = QRectF(0, 0, self.size_x, self.size_y)
+        painter.drawPixmap(target, QPixmap("../data/map.png"),source)
     def wheelEvent(self, event):
         factor = 1
-        if (self.zoom + event.delta() / 120.0) >= 0 and (self.zoom + event.delta() / 120.0) <= 5:
+        max_zoom = 5
+        self.mouse = event.pos()
+        self.zoom[0] = self.zoom[1]
+        if (self.zoom[1] + event.delta() / 120.0) >= 0 and (self.zoom[1] + event.delta() / 120.0) <= max_zoom:
             factor = pow(1.2, event.delta() / 240.0)
-            self.zoom = self.zoom + event.delta() / 120.0
-        elif (self.zoom + event.delta() / 120.0) > 5:
-            factor = pow(1.2, (5 - self.zoom) / 2)
-            self.zoom = 5
+            self.zoom[1] = self.zoom[1] + event.delta() / 120.0
+        elif (self.zoom[1] + event.delta() / 120.0) > max_zoom:
+            factor = pow(1.2, (max_zoom - self.zoom[1]) / 2)
+            self.zoom[1] = max_zoom
         else:
-            factor = pow(1.2, -self.zoom / 2)
-            self.zoom = 0
-        self.setTransform(QTransform.fromScale(factor, factor), True)
+            factor = pow(1.2, -self.zoom[1] / 2)
+            self.zoom[1] = 0
+        #self.setTransform(QTransform.fromScale(factor, factor), True)
         #self.scale(factor)
         self.update()
         super(thu_map, self).wheelEvent(event)
