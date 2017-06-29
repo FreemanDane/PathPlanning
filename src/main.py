399e702340d72460f2c58dc2bf808b7a7168c0ef
Bill Jia <jia379853818@hotmail.com>
2017-06-29 15:26:48 +0800

    Change zoom implement

--- src/main.py
+++ src/main.py
@@ -1,12 +1,77 @@
-# coding=utf-8
-from PyQt5.QtWidgets import QGraphicsView, QGraphicsScene, QApplication
-from PyQt5.QtGui import QPixmap
-import sys
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+class thu_map(QGraphicsPixmapItem, QObject):
+    def __init__(self):
+        super(thu_map, self).__init__()
+        self.setPixmap(QPixmap("../data/map.png"))
+        self.show()
+        self.setTransformationMode(Qt.SmoothTransformation)
+        self.size_x = 685.0
+        self.size_y = 831.0
+        self.zoom = [0, 0]           #第一个代表鼠标滚动之前（过去状态），第二个代表鼠标滚动之后（当前状态）
+        self.mouse = QPointF(0, 0)
+        self.top_left = QPointF(0, 0)
+        self.flag = 0
+    def boundingRect(self):
+        return QRectF(self.top_left.x(), self.top_left.y(), self.size_x * pow(1.2, 0.5 * self.zoom[1]), self.size_y * pow(1.2, 0.5 * self.zoom[1]))
+    def paint(self, painter, option, widget):
+        zoom_change = self.zoom[1] - self.zoom[0]
+        #计算x
+        if zoom_change >= 0:
+            x = self.top_left.x() - (1 - pow(1.2, -0.5 * zoom_change)) * self.mouse.x()
+        else:
+            x = (pow(1.2, 0.5 * self.zoom[1]) - 1) / (pow(1.2, 0.5 * self.zoom[0]) - 1) * self.top_left.x()
+
+        #计算y
+        if zoom_change >= 0:
+            y = self.top_left.y() - (1 - pow(1.2, -0.5 * zoom_change)) * self.mouse.y()
+        else:
+            y = (pow(1.2, 0.5 * self.zoom[1]) - 1) / (pow(1.2, 0.5 * self.zoom[0]) - 1) * self.top_left.y()
+
+        self.top_left = QPointF(x, y)
+        #target = QRectF(x, y, (self.size_x * pow(1.2, 0.5 * self.zoom[1])), (self.size_y * pow(1.2, 0.5 * self.zoom[1])))
+        if self.flag == 0:
+            self.setTransform(QTransform.fromScale(1.2, 1.2), True)
+            self.flag = 1
+        pixmap = self.pixmap()
+        target = QRectF(-200, -200, self.size_x, self.size_y)
+        source = QRectF(0, 0, self.size_x, self.size_y)
+        painter.drawPixmap(target, pixmap, source)
+    def wheelEvent(self, event):
+        factor = 1.0
+        max_zoom = 10
+        self.mouse = event.pos()
+        self.zoom[0] = self.zoom[1]
+        if (self.zoom[1] + event.delta() / 120.0) >= 0 and (self.zoom[1] + event.delta() / 120.0) <= max_zoom:
+            factor = pow(1.2, event.delta() / 240.0)
+            self.zoom[1] = self.zoom[1] + event.delta() / 120.0
+        elif (self.zoom[1] + event.delta() / 120.0) > max_zoom:
+            factor = pow(1.2, (max_zoom - self.zoom[1]) / 2)
+            self.zoom[1] = max_zoom
+        else:
+            factor = pow(1.2, -self.zoom[1] / 2)
+            self.zoom[1] = 0
+        self.setTransform(QTransform.fromScale(factor, factor), True)
+        self.update()
+        super(thu_map, self).wheelEvent(event)
+class MainWindow(QGraphicsView):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        scene = QGraphicsScene(self)
+        self.map = thu_map()
+        scene.addItem(self.map)
+        scene.setSceneRect(0, 0, 685, 831)
+        self.setScene(scene)
+        self.setCacheMode(QGraphicsView.CacheBackground)
+        self.setWindowTitle("THU MAP")
+        self.setFixedSize(687, 833)
 
 if __name__ == '__main__':
+    import sys
     app = QApplication(sys.argv)
-    scene = QGraphicsScene()
-    scene.addPixmap(QPixmap("../data/map.png"))
-    view = QGraphicsView(scene)
-    view.show()
+    mainWindow = MainWindow()
+    mainWindow.show()
     sys.exit(app.exec_())
+
--- src/map_zoom.py
+++ src/map_zoom.py
@@ -2,12 +2,14 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 
-class thu_map(QGraphicsPixmapItem):
+class thu_map(QGraphicsPixmapItem, QObject):
     def __init__(self):
         super(thu_map, self).__init__()
         self.setPixmap(QPixmap("../data/map.png"))
-        self.size_x = 685
-        self.size_y = 831
+        self.show()
+        self.setTransformationMode(Qt.SmoothTransformation)
+        self.size_x = 685.0
+        self.size_y = 831.0
         self.zoom = [0, 0]           #第一个代表鼠标滚动之前（过去状态），第二个代表鼠标滚动之后（当前状态）
         self.mouse = QPointF(0, 0)
         self.top_left = QPointF(0, 0)
@@ -15,25 +17,30 @@ class thu_map(QGraphicsPixmapItem):
         return QRectF(self.top_left.x(), self.top_left.y(), self.size_x * pow(1.2, 0.5 * self.zoom[1]), self.size_y * pow(1.2, 0.5 * self.zoom[1]))
     def paint(self, painter, option, widget):
         zoom_change = self.zoom[1] - self.zoom[0]
-        x = pow(1.2, 0.5 * zoom_change) * self.top_left.x() - (pow(1.2, 0.5 * zoom_change) - 1) * self.mouse.x()
-        if x < self.size_x * (1 - pow(1.2, 0.5 * self.zoom[1])):
-            x = self.size_x * (1 - pow(1.2, 0.5 * self.zoom[1]))
-        elif x > 0:
-            x = 0
-        y = pow(1.2, 0.5 * zoom_change) * self.top_left.y() - (pow(1.2, 0.5 * zoom_change) - 1) * self.mouse.y()
-        if y < self.size_y * (1 - pow(1.2, 0.5 * self.zoom[1])):
-            y = self.size_y * (1 - pow(1.2, 0.5 * self.zoom[1]))
-        elif y > 0:
-            y = 0
+        #计算x
+        if zoom_change >= 0:
+            x = self.top_left.x() - (pow(1.2, -0.5 * self.zoom[0]) - pow(1.2, -0.5 * self.zoom[1])) * self.mouse.x()
+        else:
+            x = (pow(1.2, 0.5 * self.zoom[1]) - 1) / (pow(1.2, 0.5 * self.zoom[0]) - 1) * self.top_left.x()
+
+        #计算y
+        if zoom_change >= 0:
+            y = self.top_left.y() - (pow(1.2, -0.5 * self.zoom[0]) - pow(1.2, -0.5 * self.zoom[1])) * self.mouse.y()
+        else:
+            y = (pow(1.2, 0.5 * self.zoom[1]) - 1) / (pow(1.2, 0.5 * self.zoom[0]) - 1) * self.top_left.y()
+
         self.top_left = QPointF(x, y)
-        target = QRectF(x, y, self.size_x * pow(1.2, 0.5 * self.zoom[1]), self.size_y * pow(1.2, 0.5 * self.zoom[1]))
+        pixmap = self.pixmap()
+        target = QRectF(x, y, self.size_x, self.size_y)
         source = QRectF(0, 0, self.size_x, self.size_y)
-        painter.drawPixmap(target, QPixmap("../data/map.png"),source)
+        painter.drawPixmap(target, pixmap, source)
     def wheelEvent(self, event):
-        factor = 1
-        max_zoom = 5
-        self.mouse = event.pos()
+        factor = 1.0
+        max_zoom = 10
+        x = event.pos().x() * pow(1.2, 0.5 * self.zoom[1])
+        y = event.pos().y() * pow(1.2, 0.5 * self.zoom[1])
         self.zoom[0] = self.zoom[1]
+        self.mouse = QPointF(x, y)
         if (self.zoom[1] + event.delta() / 120.0) >= 0 and (self.zoom[1] + event.delta() / 120.0) <= max_zoom:
             factor = pow(1.2, event.delta() / 240.0)
             self.zoom[1] = self.zoom[1] + event.delta() / 120.0
@@ -43,16 +50,22 @@ class thu_map(QGraphicsPixmapItem):
         else:
             factor = pow(1.2, -self.zoom[1] / 2)
             self.zoom[1] = 0
-        #self.setTransform(QTransform.fromScale(factor, factor), True)
-        #self.scale(factor)
+        self.setTransform(QTransform.fromScale(factor, factor), True)
         self.update()
         super(thu_map, self).wheelEvent(event)
+    def mousePressEvent(self, event):
+        x = event.pos().x() * pow(1.2, 0.5 * self.zoom[1])
+        y = event.pos().y() * pow(1.2, 0.5 * self.zoom[1])
+        self.mouse = QPointF(x, y)
+        cx = x / pow(1.2, self.zoom[1]) - self.top_left.x()
+        cy = y / pow(1.2, self.zoom[1]) - self.top_left.y()
+        a = 0
+        super(thu_map, self).mousePressEvent(event)
 class MainWindow(QGraphicsView):
     def __init__(self):
         super(MainWindow, self).__init__()
         scene = QGraphicsScene(self)
         self.map = thu_map()
-        #self.map = QPixmap("../data/map.png")
         scene.addItem(self.map)
         scene.setSceneRect(0, 0, 685, 831)
         self.setScene(scene)
